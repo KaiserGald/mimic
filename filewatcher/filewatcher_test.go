931954457892e283ff41aff7faed7f10ee73aa06
Scott Isenberg <galdwulfram@gmail.com>
2018-01-26 11:49:05 -0600

    adds some unit tests for the filewatcher

--- filewatcher/filewatcher_test.go
+++ filewatcher/filewatcher_test.go
@@ -1,6 +1,33 @@
-// Package filewatcher_test
+// Package filewatcher
 // 18 January 2018
 // Code is licensed under the MIT License
 // © 2018 Scott Isenberg
 
-package filewatcher_test
+package filewatcher
+
+import "testing"
+
+func TestWatchFiles(t *testing.T) {
+	err := WatchFiles("../filehandler/testdir/testsrc", "../filehandler/testdir/testdes")
+	if err != nil {
+		t.Errorf("Error starting filewatcher.")
+	}
+}
+
+func TestBuildPaths(t *testing.T) {
+	ep := "/home/workspace/projectroot/test/testsrc/dir/test.txt"
+	srcfp := "test/testsrc"
+	desfp := "test/testdes"
+	relfp := "/home/workspace/projectroot/test/testsrc"
+	srctest := "test/testsrc/dir/test.txt"
+	destest := "test/testdes/dir/test.txt"
+
+	src, des := buildPaths(ep, srcfp, desfp, relfp)
+	if src != srctest {
+		t.Errorf("Error building source file path. Expected '%s' got '%s'.\n", srctest, src)
+	}
+
+	if des != destest {
+		t.Errorf("Error building destination path. Expected '%s' got '%s'.\n", destest, des)
+	}
+}
